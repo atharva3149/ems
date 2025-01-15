0c5ddc7e35b80da6b19cf4dce96baf9878667635
atharva3149 <atharvanalawade3149@gmail.com>
2025-01-15 20:58:52 +0530

    employee task

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5  mt-10'>
+        <div className='flex-shrink-0 h-full w-[300px] bg-red-400 p-5  rounded-xl '>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+                <h4 className='text-sm'>20 Dec 2024</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer facilisis ex vel libero ullamcorper, a tincidunt nisi placerat. Maecenas sed ipsum ac arcu fermentum pharetra non vel nunc.
+            </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] bg-green-400 p-5  rounded-xl '>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+                <h4 className='text-sm'>20 Dec 2024</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer facilisis ex vel libero ullamcorper, a tincidunt nisi placerat. Maecenas sed ipsum ac arcu fermentum pharetra non vel nunc.
+            </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] bg-blue-400 p-5  rounded-xl '>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+                <h4 className='text-sm'>20 Dec 2024</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer facilisis ex vel libero ullamcorper, a tincidunt nisi placerat. Maecenas sed ipsum ac arcu fermentum pharetra non vel nunc.
+            </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 p-5  rounded-xl '>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+                <h4 className='text-sm'>20 Dec 2024</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer facilisis ex vel libero ullamcorper, a tincidunt nisi placerat. Maecenas sed ipsum ac arcu fermentum pharetra non vel nunc.
+            </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] bg-red-400 p-5  rounded-xl '>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+                <h4 className='text-sm'>20 Dec 2024</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer facilisis ex vel libero ullamcorper, a tincidunt nisi placerat. Maecenas sed ipsum ac arcu fermentum pharetra non vel nunc.
+            </p>
+        </div>
+
+        
+       
+    </div>
+  )
+}
+
+export default TaskList
